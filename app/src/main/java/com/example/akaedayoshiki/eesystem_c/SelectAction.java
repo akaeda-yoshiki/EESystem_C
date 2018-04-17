2106777de4e42e5dddacb489a9278bd77c2f6886
akaeda-yoshiki <y.a.05081996@gmail.com>
2018-04-18 02:00:28 +0900

    画面切り替え試行錯誤中

--- app/src/main/java/com/example/akaedayoshiki/eesystem_c/SelectAction.java
+++ app/src/main/java/com/example/akaedayoshiki/eesystem_c/SelectAction.java
@@ -3,11 +3,11 @@ package com.example.akaedayoshiki.eesystem_c;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SelectAction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.waitcard);
+        setContentView(R.layout.selectaction);
     }
 }
---
+++ app/src/main/java/com/example/akaedayoshiki/eesystem_c/WaitCard.java
@@ -0,0 +1,22 @@
+package com.example.akaedayoshiki.eesystem_c;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WaitCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.waitcard);
+
+//        findViewById(R.id.button);
+    }
+
+    public void onClick(View view){
+//        Intent intent = new Intent(this, SelectAction.class);
+//        startActivity(intent);
+    }
+}
