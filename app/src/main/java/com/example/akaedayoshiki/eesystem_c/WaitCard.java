707af695b943409d493e0b90e3b22acc08e451bb
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-04-24 11:54:43 +0900

    NFCタグのIDの読み取りに成功

--- app/src/main/java/com/example/akaedayoshiki/eesystem_c/WaitCard.java
+++ app/src/main/java/com/example/akaedayoshiki/eesystem_c/WaitCard.java
@@ -19,6 +19,25 @@ public class WaitCard extends AppCompatActivity {
     private SoundPool soundPool;
     private int cardread_wav;//読み取り時の効果音
     private NfcAdapter mNfcAdapter;
+    private PendingIntent pendingIntent;
+    //タイミングは、タグ発見時とする。
+    private IntentFilter[] intentFilter = new IntentFilter[]{
+            new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+    };
+
+    //反応するタグの種類を指定。
+    private String[][] techList = new String[][]{
+            {
+                    android.nfc.tech.NfcA.class.getName(),
+                    android.nfc.tech.NfcB.class.getName(),
+                    android.nfc.tech.IsoDep.class.getName(),
+                    android.nfc.tech.MifareClassic.class.getName(),
+                    android.nfc.tech.MifareUltralight.class.getName(),
+                    android.nfc.tech.NdefFormatable.class.getName(),
+                    android.nfc.tech.NfcV.class.getName(),
+                    android.nfc.tech.NfcF.class.getName(),
+            }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,66 +48,25 @@ public class WaitCard extends AppCompatActivity {
         soundPool       = new SoundPool( 1, AudioManager.STREAM_MUSIC, 0 );
         cardread_wav = soundPool.load(this, R.raw.waitcard, 1 );
 
-
+//NFCを見つけたときに反応させる
+        //PendingIntent→タイミング（イベント発生）を指定してIntentを発生させる
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,getClass()), 0);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        //▼▼▼▼ここから
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        //▼NFCの機能判定
-        //NFC機能なし機種
-        if(mNfcAdapter == null){
-            Toast.makeText(getApplicationContext(), "no Nfc feature", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-
-        //NFC通信OFFモード
-        if(!mNfcAdapter.isEnabled()){
-            Toast.makeText(getApplicationContext(), "off Nfc feature", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        //▲NFCの機能判定
-
-        //NFCを見つけたときに反応させる
-        //PendingIntent→タイミング（イベント発生）を指定してIntentを発生させる
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,getClass()), 0);
-
-        //タイミングは、タグ発見時とする。
-        IntentFilter[] intentFilter = new IntentFilter[]{
-                new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
-        };
-
-        //反応するタグの種類を指定。
-        String[][] techList = new String[][]{
-                {
-                        android.nfc.tech.NfcA.class.getName(),
-                        android.nfc.tech.NfcB.class.getName(),
-                        android.nfc.tech.IsoDep.class.getName(),
-                        android.nfc.tech.MifareClassic.class.getName(),
-                        android.nfc.tech.MifareUltralight.class.getName(),
-                        android.nfc.tech.NdefFormatable.class.getName(),
-                        android.nfc.tech.NfcV.class.getName(),
-                        android.nfc.tech.NfcF.class.getName(),
-                }
-        };
         mNfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, techList);
-        //▲▲▲▲▲ここまで
     }
 
     @Override
     public void onPause(){
         super.onPause();
 
-        //▼▼▼▼ここから
         //アプリが表示されてない時は、NFCに反応しなくてもいいようにする
         mNfcAdapter.disableForegroundDispatch(this);
-        //▲▲▲▲▲ここまで
     }
 
     //ボタンがタップされた時の処理
@@ -103,7 +81,6 @@ public class WaitCard extends AppCompatActivity {
     protected void onNewIntent(Intent intent){
         super.onNewIntent(intent);
 
-        //▼▼▼▼ここから
         String action = intent.getAction();
         if(TextUtils.isEmpty(action)){
             return;
@@ -113,9 +90,28 @@ public class WaitCard extends AppCompatActivity {
             return;
         }
 
-        //成功！と表示してみる
-        Toast.makeText(getApplicationContext(), "成功！", Toast.LENGTH_SHORT).show();
-        //▲▲▲▲▲ここまで
+        //NFCのIDを取得。byte配列。
+        byte[] rawId = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+        String id = bytesToString(rawId);
+        Toast.makeText(getApplicationContext(), id, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //byte配列をStringにして返す
+    public String bytesToString(byte[] bytes){
+        StringBuilder buffer = new StringBuilder();
+        boolean isFirst = true;
+
+        for(byte b : bytes){
+            if(isFirst){
+                isFirst = false;
+            } else {
+                buffer.append("-");
+            }
+            //負の場合があるので「& 0xff」をつける。
+            buffer.append(Integer.toString(b & 0xff));
+        }
+        return buffer.toString();
     }
 }
 
